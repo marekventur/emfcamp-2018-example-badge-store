3709dcc09d3859435741213b28d6523dcfc46760
Marek Ventur <marekventur@gmail.com>
2018-07-02 22:54:34 +0100

    a new game

---
+++ my_game/main.py
@@ -0,0 +1,16 @@
+"""My game"""
+
+___categories___ = ["Games"]
+___license___ = "MIT"
+
+import pyb
+import math
+import ugfx
+import buttons
+
+ugfx.init()
+buttons.init()
+buttons.disable_menu_reset()
+
+print("this is a game")
+
